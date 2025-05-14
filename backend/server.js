895fad92f5ca21360104437794bdee25fee3286c
Samik Chakraborty <115337884+Samik072@users.noreply.github.com>
2025-05-14 12:34:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,3 @@ if(process.env.NODE_ENV !== "production"){
   const port = process.env.PORT || 4000
   app.listen(port, () => console.log(`Server started on PORT:${port}`))
 }
-
-export default server;
